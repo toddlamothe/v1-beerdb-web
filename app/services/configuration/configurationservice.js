541ef92e8a4dc45b49a0cc10f9ccea834cc50205
LaMothe <Todd.LaMothe@allscripts.com>
2016-09-26 22:01:05 -0400

    HTTP/CORS issue: Head -> *bang* -> Wall

--- app/services/configuration/configurationservice.js
+++ app/services/configuration/configurationservice.js
@@ -3,7 +3,8 @@
 
    angular.module('ConfigurationService', [])
     .constant('APP_CONFIG', {
-      breweryDbBaseUrl: "http://localhost:4494/api/beerdb/"
+      breweryDbBaseUrl: "https://localhost/beerdb/api/beerdb/breweries/"
+        //breweryDbBaseUrl: "http://localhost:4494/api/beerdb/"
         // breweryDbBaseUrl: "http://api.brewerydb.com/v2/",
         // breweryDbAPIKey: " 8ee12a2f196eb183914740dbbb5ccfff" // "793f64a07905f464a63d0312cf00eefe"
     });
