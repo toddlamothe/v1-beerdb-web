d2e140cc39df850b2d657ad59d007d68d7dc44e3
Todd LaMothe <todd@ToddsMacbook-2.local>
2016-12-22 15:49:15 -0500

    Finished basic gulp build, untested

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var del = require('del');
 var minifyHtml = require('gulp-minify-html');
 var angularTemplatecache = require('gulp-angular-templatecache');
 var useref = require('gulp-useref');
+var ngAnnotate = require('gulp-ng-annotate');
 
 var config = {
   js: ['app/**/*.js', '!./app/bower_components','!./app/bower_components/**'],
@@ -27,6 +28,10 @@ var dist = {
 	fonts: 'fonts/'
 }
 
+gulp.task('build', ['minifyjs', 'copy-images'], function(){
+  del(config.temp);
+});
+
 // Lint Task
 gulp.task('lint', function() {
   console.log('linting');
@@ -35,10 +40,19 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('copy-images', function(){
+gulp.task('copy-images', ['clean-images'], function(){
   return gulp.src([config.images])
     .pipe(gulp.dest(dist.path + dist.images));
 });
+
+gulp.task('minifyjs', ['useref', 'templatecache'], function(){
+  return gulp.src(['dist/js/scripts.js', 'temp/templates.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'));
+});
+
 
 gulp.task('templatecache', function() {
   return gulp.src(config.html)
@@ -63,75 +77,12 @@ gulp.task('useref', function(){
     .pipe(gulp.dest('dist'));
 });
 
-
 //Helper Tasks
 gulp.task('clean', function() {
   del(dist.path);
   del(config.temp);
 })
 
-gulp.task('clean-images', ['clean-images'], function(){
+gulp.task('clean-images', function(){
   del(dist.path + dist.images);
 });
-
-
-
-
-
-
-
-
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src(config.js)
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('app/*.js', ['lint', 'scripts']);
-});
-
-function browserSyncInit(baseDir, files, browser) {
-  browser = browser === undefined ? 'default' : browser;
-
-  var routes = null;
-  if(baseDir === 'app' || (util.isArray(baseDir) && baseDir.indexOf('app') !== -1)) {
-    routes = {
-      // Should be '/bower_components': '../bower_components'
-      // Waiting for https://github.com/shakyShane/browser-sync/issues/308
-      '/bower_components': 'bower_components'
-    };
-  }
-
-  browserSync.instance = browserSync.init(files, {
-    startPath: '/index.html',
-    server: {
-      baseDir: baseDir,
-      middleware: middleware,
-      routes: routes
-    },
-    browser: browser
-  });
-
-};
-
-gulp.task('serve', ['watch'], function () {
-  browserSyncInit([
-    'app',
-    '.tmp'
-  ], [
-    '.tmp/{app,components}/**/*.css',
-    'app/assets/images/**/*',
-    'app/*.html',
-    'app/{app,components}/**/*.html',
-    'app/{app,components}/**/*.js'
-  ]);
-});
-
-// Default Task
-gulp.task('default', ['lint', 'scripts', 'watch']);
