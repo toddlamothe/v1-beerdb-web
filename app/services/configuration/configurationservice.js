8993a38b562fe250d924dbbf9fad54143f180f78
Todd LaMothe <todd.lamothe@gmail.com>
2017-02-01 08:41:27 -0500

    Attempting fix for Google API issue

--- app/services/configuration/configurationservice.js
+++ app/services/configuration/configurationservice.js
@@ -5,7 +5,7 @@
     .constant('APP_CONFIG', (function configurationBuilder() {
       var _environment = "techfest";
       var _version = 0.1;
-      var _build = 11;
+      var _build = 12;
       var _environments = {
           test: {
             config: {
