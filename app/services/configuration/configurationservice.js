4e731c0b2afa8563092a6ff19c0c248b23246585
Todd LaMothe <todd.lamothe@gmail.com>
2017-04-20 11:20:12 -0400

    Updated build #

--- app/services/configuration/configurationservice.js
+++ app/services/configuration/configurationservice.js
@@ -5,7 +5,7 @@
     .constant('APP_CONFIG', (function configurationBuilder() {
       var _environment = "azure";
       var _version = 0.1;
-      var _build = 16;
+      var _build = 18;
       var _environments = {
           test: {
             config: {
